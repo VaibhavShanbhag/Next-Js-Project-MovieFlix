2a835c0749ef1aca9233a31b2f4f320144095cdb
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2024-03-14 11:14:35 +0530

    Fixed the apos error in contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -13,7 +13,7 @@ const Page = () => {
         <ContactCard />
 
         <section className={styles.contact_section}>
-          <h2>We'd love to hear <span> from you </span></h2>
+          <h2>We&apos;d love to hear <span> from you </span></h2>
 
           <ContactForm />
         </section>
